0000ec16af82d528412309f83dd4f5d4b47d1367
cx <1174363087@qq.com>
2019-02-14 12:59:19 +0800

    ''

--- src/main/java/com/chenxin/application/Application.java
+++ src/main/java/com/chenxin/application/Application.java
@@ -1,9 +1,13 @@
 package com.chenxin.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.chenxin")
 public class Application {
     public static void main(String args[]){
         SpringApplication.run(Application.class,args);
--- src/main/java/com/chenxin/controller/TestWeiXinToken.java
+++ src/main/java/com/chenxin/controller/TestWeiXinToken.java
@@ -1,14 +1,21 @@
 package com.chenxin.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestWeiXinToken {
-    @RequestMapping("/confirm")
-    public String confirm(@RequestParam("signature") String signature ,@RequestParam("token") String token ,
-                          @RequestParam("timestamp") String timestamp ,@RequestParam("nonce") String nonce){
+    @RequestMapping("/test")
+    public String confirm(){
+        return "signature";
+    }
+
+    @RequestMapping(value = "/confirm" , method = RequestMethod.GET)
+    public String confirm(@RequestParam(value = "signature" , required = true) String signature ,@RequestParam(value = "token" , required = true) String token ,
+                          @RequestParam(value = "timestamp" , required = true) String timestamp ,@RequestParam(value = "nonce" , required = true) String nonce){
         System.out.println("hello");
         return signature;
     }
