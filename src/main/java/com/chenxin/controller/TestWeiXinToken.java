18100d9ad34c2a6a1e49707f27bd6f23ddeb5c2b
cx <1174363087@qq.com>
2019-02-14 11:21:49 +0800

    ''

--- src/main/java/com/chenxin/controller/TestWeiXinToken.java
+++ src/main/java/com/chenxin/controller/TestWeiXinToken.java
@@ -9,8 +9,7 @@ public class TestWeiXinToken {
     @RequestMapping("/confirm")
     public String confirm(@RequestParam("signature") String signature ,@RequestParam("token") String token ,
                           @RequestParam("timestamp") String timestamp ,@RequestParam("nonce") String nonce){
-
+        System.out.println("hello");
         return signature;
-
     }
 }
