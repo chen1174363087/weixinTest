950163e1122b144849e04332c7d5e9b3ecd28d47
cx <1174363087@qq.com>
2019-02-14 14:58:47 +0800

    ''

--- src/main/java/com/chenxin/controller/TestWeiXinToken.java
+++ src/main/java/com/chenxin/controller/TestWeiXinToken.java
@@ -1,22 +1,85 @@
 package com.chenxin.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 @RestController
 public class TestWeiXinToken {
     @RequestMapping("/test")
-    public String confirm(){
+    public String confirm() {
         return "signature";
     }
 
-    @RequestMapping(value = "/confirm" , method = RequestMethod.GET)
-    public String confirm(@RequestParam(value = "signature" , required = true) String signature ,@RequestParam(value = "token" , required = true) String token ,
-                          @RequestParam(value = "timestamp" , required = true) String timestamp ,@RequestParam(value = "nonce" , required = true) String nonce){
+    @RequestMapping(value = "/confirm", method = RequestMethod.GET)
+    public String confirm(@RequestParam(value = "signature", required = true) String signature,
+                          @RequestParam(value = "timestamp", required = true) String timestamp, @RequestParam(value = "nonce", required = true) String nonce) {
         System.out.println("hello");
         return signature;
+    }
+
+    @RequestMapping(value = "/confirm1", method = RequestMethod.GET)
+    public String confirm(HttpServletRequest request, HttpServletResponse response) {
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        if (checkSignature(signature, timestamp, nonce)) {
+            return signature;
+        }
+        return null;
+    }
+
+    public boolean checkSignature(String signature, String timestamp,
+                                  String nonce) {
+        // 1.将token、timestamp、nonce三个参数进行字典序排序
+        String[] arr = new String[]{"chenxin520", timestamp, nonce};
+        Arrays.sort(arr);
+
+        // 2. 将三个参数字符串拼接成一个字符串进行sha1加密
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            content.append(arr[i]);
+        }
+        MessageDigest md = null;
+        String tmpStr = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            // 将三个参数字符串拼接成一个字符串进行sha1加密
+            byte[] digest = md.digest(content.toString().getBytes());
+            tmpStr = byteToStr(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        content = null;
+        // 3.将sha1加密后的字符串可与signature对比，标识该请求来源于微信
+        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
+    }
+
+    private String byteToStr(byte[] byteArray) {
+        StringBuilder strDigest = new StringBuilder();
+        for (int i = 0; i < byteArray.length; i++) {
+            strDigest.append(byteToHexStr(byteArray[i]));
+        }
+        return strDigest.toString();
+    }
+
+    private String byteToHexStr(byte mByte) {
+        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+                'B', 'C', 'D', 'E', 'F'};
+        char[] tempArr = new char[2];
+        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = Digit[mByte & 0X0F];
+        String s = new String(tempArr);
+        return s;
     }
 }
