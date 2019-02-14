b091e3e31ea76999698e24eb971f09c3a0e3e868
cx <1174363087@qq.com>
2019-02-14 10:47:10 +0800

    ''

---
+++ src/main/java/com/chenxin/controller/TestWeiXinToken.java
@@ -0,0 +1,16 @@
+package com.chenxin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestWeiXinToken {
+    @RequestMapping("/confirm")
+    public String confirm(@RequestParam("signature") String signature ,@RequestParam("token") String token ,
+                          @RequestParam("timestamp") String timestamp ,@RequestParam("nonce") String nonce){
+
+        return signature;
+
+    }
+}
